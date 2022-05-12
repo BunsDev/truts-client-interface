795b3b1169e47acc9609aa94a934b9b62c768ecd
johnswaroop <71548636+johnswaroop@users.noreply.github.com>
2022-05-12 12:44:36 +0530

    dao list page

--- pages/dao-list/index.jsx
+++ pages/dao-list/index.jsx
@@ -114,7 +114,7 @@ function DaoList() {
                                     />
                                 }
                             }
-                        })
+                        }).reverse()
                     }
                 </div>
             </div>
