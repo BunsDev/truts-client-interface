a85f43e11d72c068371ead007d4695f3560de62e
johnswaroop <71548636+johnswaroop@users.noreply.github.com>
2022-06-02 00:40:38 +0530

    meta data change

--- pages/index.jsx
+++ pages/index.jsx
@@ -73,7 +73,7 @@ export default function Home({ daoList_ssr, leaderboard_ssr }) {
             <h3>Review web3 communities,</h3>
             <h3 className={styles.titleBlue}>Earn Rewards!</h3>
           </div>
-          <p className={styles.subTitle}>Discover cool communities Learn, Contribute and Earn with communities you love and Truts</p>
+          <p className={styles.subTitle}>Discover web3 communities that vibes with you from a list of thousands of communities across different categories (service, investment, media, social) and know all about them</p>
           <SearchComp data={daoList} />
         </div>
 
