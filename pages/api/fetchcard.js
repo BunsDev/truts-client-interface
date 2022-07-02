08e96424c012f28e0c4574afcd5b4a55827371d0
johnswaroop <71548636+johnswaroop@users.noreply.github.com>
2022-07-02 21:02:12 +0530

    api change

--- pages/api/fetchcard.js
+++ pages/api/fetchcard.js
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import axios from "axios"
-let API = `http://35.88.248.101:5500`
+let API = `https://rsqt69nmq4.us-west-2.awsapprunner.com`
 export default async function handler(req, res) {
   let rid = req.query.rid
   let res_bin = await axios.get(`${API}/review-card-bin/?rid=${rid}`);
