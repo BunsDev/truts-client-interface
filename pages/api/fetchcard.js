c95892ef8b1a29e277a2442c652a3f92f75b180a
johnswaroop <71548636+johnswaroop@users.noreply.github.com>
2022-07-03 14:15:10 +0530

    proper screenshot

--- pages/api/fetchcard.js
+++ pages/api/fetchcard.js
@@ -3,7 +3,7 @@ import axios from "axios"
 let API = `https://rsqt69nmq4.us-west-2.awsapprunner.com`
 export default async function handler(req, res) {
   let rid = req.query.rid
-  let res_bin = await axios.get(`${API}/review-card-bin/?rid=${rid}`);
+  let res_bin = await axios.get(`${API}/review-snapshot?rid=${rid}`);
   let img = Buffer.from(res_bin.data.bin, 'utf8');
   res.writeHead(200, { 'Content-Type': 'image/png' });
   res.end(img, 'binary');
