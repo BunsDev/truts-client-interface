9cb459a7fb725e7d9ac74035bda143a009a5124c
Ishan Pandey <191.imp@gmail.com>
2022-07-14 19:11:23 +0530

    Updating WAGMI as a word in Share Module

--- pages/dao/[...slug].jsx
+++ pages/dao/[...slug].jsx
@@ -1245,7 +1245,7 @@ function Comment({ comment, address, rating, profile_img, openModel, data, openC
                     <p>{(data.chain == 'sol') ? 'Tip SOL' : 'Tip MATIC'}</p>
                 </span>
                 { <span>
-                    <img src="/share.png" alt="" onClick={() => { openNewTab(`http://twitter.com/share?text=Check out this ${data.rating}⭐ review for ${data.dao_name} (@${twitter_slug}) on @trutsxyz  &hashtags=truts,${unspacedDaoName},WAGMI &url=https://www.truts.xyz/dao/${slug}/${data._id}`) }} />
+                    <img src="/share.png" alt="" onClick={() => { openNewTab(`http://twitter.com/share?text=Check out this ${data.rating}⭐ review for ${data.dao_name} (@${twitter_slug}) on @trutsxyz  &hashtags=truts,${unspacedDaoName} %0WAGMI &url=https://www.truts.xyz/dao/${slug}/${data._id}`) }} />
                 </span> }
             </div>
         </div>
