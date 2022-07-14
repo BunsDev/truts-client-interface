b2ce4a5600ab74a7adc7916f5bc3d2b3bf276ed7
Ishan Pandey <191.imp@gmail.com>
2022-07-14 15:02:12 +0530

    Updating Share Module Twitter Content

--- pages/dao/[...slug].jsx
+++ pages/dao/[...slug].jsx
@@ -1244,7 +1244,7 @@ function Comment({ comment, address, rating, profile_img, openModel, data, openC
                     <p>{(data.chain == 'sol') ? 'Tip SOL' : 'Tip MATIC'}</p>
                 </span>
                 { <span>
-                    <img src="/share.png" alt="" onClick={() => { openNewTab(`http://twitter.com/share?text=Discovering ${data.dao_name} (@${twitter_slug}) on Truts.xyz (@trutsxyz) &url=https://www.truts.xyz/dao/${slug}/${data._id}`) }} />
+                    <img src="/share.png" alt="" onClick={() => { openNewTab(`http://twitter.com/share?text=Check out this ${data.rating}⭐ review for ${data.dao_name} (@${twitter_slug}) on Truts.xyz (@trutsxyz) &url=https://www.truts.xyz/dao/${slug}/${data._id}`) }} />
                 </span> }
             </div>
         </div>
